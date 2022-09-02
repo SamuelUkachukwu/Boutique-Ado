5caf1eb63415bed2a2797e4b07aceadadf43bbb3
SamuelUkachukwu <shybold@yahoo.com>
2022-09-02 19:18:30 +0000

    Add checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
 import stripe
+import json
 
 from bag.contexts import bag_contents
 from products.models import Product
@@ -10,8 +12,24 @@ from .models import Order, OrderLineItem
 from .forms import OrderForm
 
 
-def checkout(request):
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
+
+def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -39,7 +57,7 @@ def checkout(request):
                         order_line_item = OrderLineItem(
                             order=order,
                             product=product,
-                            quantity=item_data
+                            quantity=item_data,
                         )
                         order_line_item.save()
                     else:
@@ -51,24 +69,22 @@ def checkout(request):
                                 product_size=size,
                             )
                             order_line_item.save()
-                except product.DoesNotExist:
+                except Product.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your bag wasnt found in our database. "
+                        "One of the products in your bag wasn't found in our database. "
                         "Please call us for assistance!")
-
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
-            request.session['save_info'] = 'save-info in request.POST'
+
+            request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
-                Please double check your information')
+                Please double check your information.')
     else:
-
         bag = request.session.get('bag', {})
         if not bag:
-
             messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('products'))
 
@@ -78,19 +94,20 @@ def checkout(request):
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY
+            currency=settings.STRIPE_CURRENCY,
         )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
-             Did you forget to set it in your enviroment?')
+            Did you forget to set it in your environment?')
 
-    order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
@@ -98,11 +115,11 @@ def checkout(request):
 
 def checkout_success(request, order_number):
     """
-    Handle succesful checkout
+    Handle successful checkouts
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Order succesfully processed! \
+    messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
 
